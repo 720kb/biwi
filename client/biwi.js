e86a531f6782691d3b7014d558ac3cebf26b5f39
45kb <filippo.oretti@gmail.com>
2015-04-23 11:14:02 +0200

    linted, need a check

--- client/biwi.js
+++ client/biwi.js
@@ -1,82 +1,87 @@
 var bitcore = require('bitcore')
-// var _ = require('lodash')
+//, _ = require('lodash')
+  , oReq
+  , ajax = {
+      'get': function get(url) {
 
-var ajax = {
+        function reqListener() {
 
-  get: function get(url) {
-    function reqListener () {
-      console.log(this.responseText);
+          console.log(this.responseText);
+        }
+
+        oReq = new XMLHttpRequest();
+        oReq.onload = reqListener;
+        oReq.open('get', url, true);
+        oReq.send();
+      }
     }
-
-    var oReq = new XMLHttpRequest();
-    oReq.onload = reqListener;
-    oReq.open("get", url, true);
-    oReq.send();
-  }
-
-}
-
+  , rootPrivateKey
+  , unspentOutput
+  , newInput
+  , nav = document.querySelector('.nav')
+  , btcAmount = 50 // mbtc
+  , costPerMin = 0.1 // mbtc
+  , remainingMinutes = btcAmount / costPerMin
+  , balanceUrl = 'https://blockchain.info/q/addressbalance/19e2eU15xKbM9pyDwjFsBJFaSeKoDxp8YT';
 
 // Dario's way--
 
 // // biwi = new Biwi()
 // biwi.init()
 // biwi.privateKey_Generate()
-
 // var BiWi = function Biwi() {
 //   biwi.init = function init() {
-//
 //   }
 //   biwi.privateKey_Generate = function privateKey_Generate() {
-//
 //   }
-//
 // }
-
-
 /////////////////////////////////
 // base js  (basic way)
 
 // main.js
 //
 
-init()
-console.log("localstorage:", localStorage)
-
+init();
+console.log('localstorage:', localStorage);
 
 // lib/biwi.js
 //
 
 function init() {
-  initStorage()
-  checkOrGeneratePrivateKey()
+
+  initStorage();
+  checkOrGeneratePrivateKey();
   //loadLastAddress()
 }
 
 function initStorage() {
-  if (!localStorage.depth)
-    localStorage.depth = 0
+
+  if (!localStorage.depth) {
+
+    localStorage.depth = 0;
+  }
   // pvt key ....
 }
 
 function incrementDepth() {
-  localStorage.depth++
+
+  localStorage.depth += 1;
 }
 
 function checkOrGeneratePrivateKey() {
-  var rootPrivateKey
 
   if (localStorageNotPresent()) {
-    rootPrivateKey = generateRootPrivateKey()
-    localStorage.rootPrivateKey = rootPrivateKey.toString()
-    incrementDepth()
+
+    rootPrivateKey = generateRootPrivateKey();
+    localStorage.rootPrivateKey = rootPrivateKey.toString();
+    incrementDepth();
     // return
-    checkOrGeneratePrivateKey()
-  }
-  else {
-    depth           = localStorage.depth
-    rootPrivateKey  = generateRootPrivateKey(localStorage.privateKey)
-    childPrivateKey = loadLastChildPrivateKey(localStorage.depth, rootPrivateKey)
+    checkOrGeneratePrivateKey();
+  } else {
+
+    depth = localStorage.depth;
+    rootPrivateKey = generateRootPrivateKey(localStorage.privateKey);
+    childPrivateKey = loadLastChildPrivateKey(localStorage.depth, rootPrivateKey);
     //childPrivateKey = loadLastChildPrivateKey(rootPprivaterivateKey)
   }
 
@@ -85,166 +90,128 @@ function checkOrGeneratePrivateKey() {
 }
 
 function localStorageNotPresent() {
-  return !localStorage.rootPrivateKey
+
+  return !localStorage.rootPrivateKey;
 }
 
 function loadLastChildPrivateKey(depth, rootPrivateKey) {
-  return rootPrivateKey.derive("m/0/"+depth)
+
+  return rootPrivateKey.derive('m/0/' + depth);
 }
 
 function loadLastChildPublicKey(depth, rootPrivateKey) {
-  return rootPrivateKey.derive("m/0/"+depth).hdPublicKey
-    .publicKey.toAddress().toString()
+
+  return rootPrivateKey.derive('m/0/' + depth).hdPublicKey.publicKey.toAddress().toString();
 }
 
 function generateRootPrivateKey(keyString) {
-  return new bitcore.HDPrivateKey(keyString)
+
+  return new bitcore.HDPrivateKey(keyString);
 }
 
-function getChangeAddress(){
-  rootPrivateKey = localStorage.rootPrivateKey
-  changeAddress = rootPrivateKey.derive("m/0/"+(depth+1))
-  return changeAddress
+function getChangeAddress() {
+
+  rootPrivateKey = localStorage.rootPrivateKey;
+  changeAddress = rootPrivateKey.derive('m/0/' + (depth + 1));
+  return changeAddress;
 }
 
-getUnspentOutput("19e2eU15xKbM9pyDwjFsBJFaSeKoDxp8YT")
+getUnspentOutput('19e2eU15xKbM9pyDwjFsBJFaSeKoDxp8YT');
 
 function getUnspentOutput(address) {
-  ajax.get("https://blockchain.info/unspent?active="+address+"&format=json&cors=true", function(utxos){
-    // console.log("utxos", utxos)
+
+  ajax.get('https://blockchain.info/unspent?active=' + address + '&format=json&cors=true', function(utxos) {
+    // console.log('utxos', utxos)
     //
-    // "tx_hash":"31fbdf5e9730e1....427b58effb837",
-    //   "tx_index":84076350,
-    //   "tx_output_n": 1,
-    //   "script":"76a9145ec1c5554a111386...c15dabe4d5e388ac",
-    //   "value": 8928186,
+    // 'tx_hash':'31fbdf5e9730e1....427b58effb837',
+    //   'tx_index':84076350,
+    //   'tx_output_n': 1,
+    //   'script':'76a9145ec1c5554a111386...c15dabe4d5e388ac',
+    //   'value': 8928186,
 
-
-
-
-    var unspent_output = getFirstUTXO(utxos)
-
-    var new_input = {
-     address:      address,
-     txid:         unspent_output.tx_hash,
-     scriptPubKey: unspent_output.script,
-     amount:       unspent_output.value,
-     vout:         unspent_output.tx_output_n,
-   }
-
-   return new_input
+    unspentOutput = getFirstUTXO(utxos);
+    /*eslint disable*/
+    newInput = {
+      'address': address,
+      'txid': unspentOutput.tx_hash,
+      'scriptPubKey': unspentOutput.script,
+      'amount': unspentOutput.value,
+      'vout': unspentOutput.tx_output_n,
+    };
+    /*eslint enable*/
+   return newInput;
   })
 
 }
 
 function getFirstUTXO(utxos) {
-  return utxos[0]
+
+  return utxos[0];
 }
 
 function payToServer(address, childPrivateKey, amount){
-  changeAddress = getChangeAddress()
 
-
+  changeAddress = getChangeAddress();
     // return utxos
-
   // createAndSignTx(address, utxos)
-
     // createTransaction
     // var transaction = new Transaction()
     //   .from(utxos)          // Feed information about what unspent outputs one can use
     //   .to(address, amount)  // Add an output with the given amount of satoshis
     //   .change(changeAddress)      // Sets up a change address where the rest of the funds will go
     //   .sign(privkeySet)     // Signs all the inputs it can
-
   // pushTx()
-
   // ....
-  incrementDepth()
+  incrementDepth();
 }
 
-
-
 // prezzo fisso
-
 // x per MB
 // x per tempo (peggio x QoS?)
-
 ///////
-
 // applicazioni Client / Server
-
 // bisogna non far comunicare le applicazioni perche'
-
 // di modo che siano indipendnti e possano:
-
 // 1 - essere swappate (implementazione in altri linguaggi / metodologie etc etc etc)
 // 2 - semplicita' (meno api call)
 // 3 - tutto e' push
-
-
 // il client riconosce le reti biwi perche' il nome della rete biwi inizia con biwi
-
 // ovviamente x l'hackathon non avremo maaaai il tempo di fare sta roba quindi alla fine forniremo all'utente solamente l'accesso vpn on pagee lui se lo setta da solo
-//
 // nota: quando dovrai rimuovere questo testo perche' avrai finito l'app allora ok
-
-
 // per settare i (amount fiat) / GB
-
 // il server nomina la rete biwi_10_(rand), e il client in questo modo sa quanto deve pagare
-
-//
-
-
 // [server] -> [rete biwi_10_antani]
-
-
-
 ////////////
 // fake events
 
-var nav = document.querySelector(".nav")
-
-// test data
-var btc_amount        = 50 // mbtc
-var cost_per_min      = 0.1 // mbtc
-var remaining_minutes = btc_amount / cost_per_min
-
 setInterval(function(){
-  btc_amount = btc_amount - cost_per_min
-  btc_amount_rounded = parseInt(btc_amount * 10) / 10
-  remaining_minutes  = parseInt(btc_amount / cost_per_min)
 
-  var evt = new CustomEvent('tx_sent',
-    {
-      detail: {
-        remaining_mbtc:    btc_amount_rounded,
-        remaining_minutes: remaining_minutes
-      }
+  btcAmount = btcAmount - costPerMin;
+  btcAmountRounded = Number(btcAmount * 10) / 10;
+  remainingMinutes  = Number(btcAmount / costPerMin);
+
+  nav.dispatchEvent(new CustomEvent('tx:sent',{
+    'detail': {
+      'remainingMbtc': btcAmountRounded,
+      'remainingMinutes': remainingMinutes
     }
-  )
-  nav.dispatchEvent(evt);
-}, 1000) // this should be 60000
+  }));
+}, 1000); // this should be 60000
 
 
-nav.addEventListener("tx_sent", function(evt){
-  var data = evt.detail
-  console.log("remaining_mbtc:", data.remaining_mbtc, "remaining_minutes:", data.remaining_minutes)
+nav.addEventListener('tx:sent', function(evt) {
+
+  console.log('remainingMbtc:', evt.detail.remainingMbtc, 'remainingMinutes:', evt.detail.remainingMinutes);
   // TODO Pippo: update view here
-})
-
-
-
-
+});
 
 /////////////
 
 // get address balance
-var balance_url = "https://blockchain.info/q/addressbalance/19e2eU15xKbM9pyDwjFsBJFaSeKoDxp8YT"
-ajax.get(balance_url, function(data){
-  return data
-})
+ajax.get(balanceUrl, function(data) {
 
+  return data;
+});
 
-address_input = document.querySelector("input[name=btc_address]")
-address_input.innerHTML = localStorage.addresses.m_0_1
+addressInput = document.querySelector('input[name=btcAddress]');
+addressInput.innerHTML = localStorage.addresses.m_0_1;
